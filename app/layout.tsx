1e7dd81ed18ddda557dc7f8ee6efc027e5298896
laricko <ttttmmm6432@gmail.com>
2025-11-05 16:45:42 +0300

    project card fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       >
         <SettingsProvider>
           <Header />
-          <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <main className="flex min-h-screen flex-col items-center justify-between p-20">
             {children}
           </main>
         </SettingsProvider>
